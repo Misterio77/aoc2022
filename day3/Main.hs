6b28c5e927236a3eaf850dc8d98063e3ead50a47
Gabriel Fontes <hi@m7.rs>
2022-12-04 03:15:04 -0300

    simplify day 3

--- day3/Main.hs
+++ day3/Main.hs
@@ -1,16 +1,48 @@
 module Main where
 
+import Data.Char (isLower, isUpper, ord)
+import Data.List (group)
 import Data.List.Split (chunksOf)
-import qualified Rucksack
+import GHC.List (splitAt)
+
+dedup :: (Eq a) => [a] -> [a]
+dedup = map head . group
+
+type Sack = String
+
+type Item = Char
+
+-- Get item priority value
+itemPriority :: Item -> Int
+itemPriority x
+  | isUpper x = ord x - 38
+  | isLower x = ord x - 96
+  | otherwise = error ("Invalid item: " ++ show x)
+
+-- Given group of items, get total priority sum
+totalPriority :: [Item] -> Int
+totalPriority = sum . map itemPriority
+
+-- Get common items (items present in both halves of the given sack)
+inBothPockets :: Sack -> [Item]
+inBothPockets sack = dedup $ filter (`elem` pocket1) pocket2
+  where
+    middle = length sack `div` 2
+    (pocket1, pocket2) = splitAt middle sack
+
+-- Get badge items (items present in all of the given sacks)
+getBadges :: [Sack] -> [Item]
+getBadges [] = [] -- No sacks, no badges
+getBadges (firstSack : otherSacks) = dedup $ filter isInAllSacks firstSack
+  where
+    isInAllSacks i = all (i `elem`) otherSacks
 
 main :: IO ()
 main = do
-  inputLines <- lines <$> getContents
-  let rucksacks = map Rucksack.fromString inputLines
+  sacks <- lines <$> getContents
   -- Part 1
-  let priorities = concatMap Rucksack.getPriorities rucksacks
-  print $ sum priorities
+  let commonItems = concatMap inBothPockets sacks
+  print $ totalPriority commonItems
   -- Part 2
-  let badges = map Rucksack.getBadge $ chunksOf 3 rucksacks
-  let priorities = map Rucksack.itemPriority badges
-  print $ sum priorities
+  let badgeItems = concatMap getBadges $ chunksOf 3 sacks
+  print $ totalPriority badgeItems
