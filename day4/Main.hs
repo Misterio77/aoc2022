dcf1f3e0fce2f84b9b68d85b7947ed3612e2f4fb
Gabriel Fontes <hi@m7.rs>
2022-12-04 16:47:43 -0300

    add day4 part 2

--- day4/Main.hs
+++ day4/Main.hs
@@ -3,6 +3,9 @@ module Main where
 import Data.Char (digitToInt)
 import Data.List.Split (splitOneOf)
 
+contains :: Eq a => ([a], [a]) -> Bool
+contains (range1, range2) = any (`elem` range1) range2 || any (`elem` range2) range1
+
 fullyContains :: Eq a => ([a], [a]) -> Bool
 fullyContains (range1, range2) = all (`elem` range1) range2 || all (`elem` range2) range1
 
@@ -14,4 +17,8 @@ rangesFromLine input = ([(parsed !! 0) .. (parsed !! 1)], [(parsed !! 2) .. (par
 main :: IO ()
 main = do
   inputLines <- lines <$> getContents
-  print $ sum $ map (fromEnum . fullyContains . rangesFromLine) inputLines
+  let ranges = map rangesFromLine inputLines
+  -- Part 1
+  print $ sum $ map (fromEnum . fullyContains) ranges
+  -- Part 2
+  print $ sum $ map (fromEnum . contains) ranges
